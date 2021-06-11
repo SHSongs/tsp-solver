ffb40ed4fe532c36a778732c79cf4399cc041d55
Song <217.Songs.SH@gmail.com>
2021-06-11 23:21:57 +0900

    Active search, losses, episode length

--- tsp-v1-solver.py
+++ tsp-v1-solver.py
@@ -3,6 +3,9 @@ import or_gym
 import torch
 import torch.optim as optim
 from pointer_network import PointerNetwork
+import matplotlib.pyplot as plt
+
+train_mode = "active-search"
 
 episode = 1000
 seq_len = 7
@@ -10,15 +13,22 @@ seq_len = 7
 env_config = {'N': seq_len}
 env = or_gym.make('TSP-v1', env_config=env_config)
 
+# Pointer network hyper parameter
 embedding_size = 128
 hidden_size = 128
-
+grad_clip = 1.5
 learning_rate = 3e-4
 model = PointerNetwork(embedding_size, hidden_size, seq_len, n_glimpses=2, tanh_exploration=10)
 
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+losses = []
 episodes_length = []
+
+# Active search hyper parameter
+beta = 0.99
+moving_avg = torch.zeros(1)
+first_step = True
 
 for i in range(episode):
     s = env.reset()
@@ -51,3 +61,34 @@ for i in range(episode):
 
     episodes_length.append(total_reward)
     print('total length', total_reward)
+
+    if first_step:  # generating first baseline
+        moving_avg = total_reward
+        first_step = False
+        continue
+
+    moving_avg = moving_avg * beta + total_reward * (1.0 - beta)
+    advantage = total_reward - moving_avg
+    log_probs = torch.sum(log_probs)
+    log_probs[log_probs < -100] = - 100
+    loss = advantage * log_probs
+
+    losses.append(loss.item())
+    print('loss : ', loss.item())
+
+    torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+plt.plot(range(len(losses)), losses, color="blue")
+plt.title(train_mode + " losses")
+plt.xlabel("episode")
+plt.ylabel("loss")
+plt.show()
+
+plt.plot(range(len(episodes_length)), episodes_length, color="blue")
+plt.title("Episode length")
+plt.xlabel("episode")
+plt.ylabel("length")
+plt.show()
